ca9db56e9ca637e4039c34319d0fd60e13e8f01a
Roman <romanbychko84@gmail.com>
2024-07-29 20:13:56 +0300

    removed file load from MailAttachment implementation

--- src/MailAttachment.cpp
+++ src/MailAttachment.cpp
@@ -6,30 +6,7 @@
 
 namespace ISXSC
 {
-    MailAttachment::MailAttachment(const std::filesystem::path& path): m_path(path)
-    {
-        std::ifstream file(path, std::ios::binary);
-        if (!file.is_open())
-        {
-            throw std::runtime_error("Failed to open file");
-        }
-
-        // determine file size in advance
-        file.seekg(0, std::ios::end);
-        std::streampos length = file.tellg();
-        file.seekg(0, std::ios::beg);
-
-        if (length > S_MAX_SIZE)
-        {
-            file.close();
-            throw std::runtime_error("File is too large");
-        }
-
-        m_data.reserve(length);
-        m_data.assign(std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>()));
-
-        file.close();
-    }
+    MailAttachment::MailAttachment(const std::filesystem::path& path): m_path(path){}
 
     std::filesystem::path MailAttachment::get_path() const
     {
@@ -39,10 +16,5 @@ namespace ISXSC
     std::string MailAttachment::get_name() const
     {
         return m_path.filename().string();
-    }
-
-    std::string MailAttachment::get_data() const
-    {
-        return m_data;
     }
 }
