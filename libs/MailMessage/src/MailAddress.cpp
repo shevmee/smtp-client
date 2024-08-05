e63adb0638d1b50ba648b6ed37589e66b7f2c6ab
Roman <romanbychko84@gmail.com>
2024-08-05 16:36:51 +0300

    removed redundant operator

--- libs/MailMessage/src/MailAddress.cpp
+++ libs/MailMessage/src/MailAddress.cpp
@@ -2,20 +2,7 @@
 
 namespace ISXSC
 {
-
     MailAddress::MailAddress(const std::string& address, const std::string& name): m_address(address), m_name(name){}
-    
-    MailAddress::operator std::string() const
-    {
-        if (!m_name.empty())
-        {
-            return m_name + " <" + m_address + ">";
-        }
-        else 
-        {
-            return m_address;
-        }
-    }
 
     std::string MailAddress::get_address() const
     {
