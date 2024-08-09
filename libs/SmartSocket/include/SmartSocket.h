14418108733c1be70b5c52e6589ba64e7065aa27
vsh51 <volsp125@gmail.com>
2024-08-09 13:43:30 +0300

    SmartSocket implementation

---
+++ libs/SmartSocket/include/SmartSocket.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/asio/spawn.hpp>
+
+#include <string>
+#include <sstream>
+#include <regex>
+#include <future>
+
+using namespace boost;
+
+using std::string;
+using asio::ip::tcp;
+using std::function;
+
+namespace ISXSmartSocket
+{
+    class SmartSocket
+    {
+    public:
+        SmartSocket(asio::io_context& io_context, asio::ssl::context& ssl_context);
+        ~SmartSocket();
+        
+        bool IsOpen() const;
+        string GetLocalname() const;
+        int GetLocalPort() const;
+        string GetServername() const;
+        int GetServerPort() const;
+
+        bool SetTimeout(int timeout);
+
+        boost::asio::io_context& GetIoContext();
+
+        bool AsyncConnectCoroutine(const string& server, int port, asio::yield_context& yield);
+        bool AsyncWriteCoroutine(const string& data, asio::yield_context& yield);
+        string AsyncReadCoroutine(asio::yield_context& yield);
+        bool AsyncUpgradeSecurityCoroutine(asio::yield_context& yield);
+        bool Close();
+
+    private:
+        string m_server;
+        int m_port;
+
+        bool m_ssl_enabled;
+
+        int m_timeout = 5;
+
+        tcp::resolver m_resolver;
+        asio::io_context& m_io_context;
+        asio::ssl::context& m_ssl_context;
+        asio::ssl::stream<tcp::socket> m_socket;
+        
+        std::unique_ptr<asio::steady_timer> StartTimer(
+            int seconds, asio::yield_context& yield, system::error_code& ec);
+    };
+}; // namespace ISXSmartSocket
---
+++ libs/SmartSocket/src/SmartSocket.cpp
@@ -0,0 +1,149 @@
+#include "SmartSocket.h"
+#include "Handlers.h"
+
+#include <iostream>
+#include <boost/asio/steady_timer.hpp>
+#include <memory>
+
+namespace ISXSmartSocket
+{
+    SmartSocket::SmartSocket(
+        asio::io_context& io_context
+        , asio::ssl::context& ssl_context):
+        m_io_context(io_context)
+        , m_ssl_context(ssl_context)
+        , m_resolver(io_context)
+        , m_socket(io_context, ssl_context)
+        , m_ssl_enabled(false) {};
+
+    SmartSocket::~SmartSocket()
+    {
+        Close();
+    };
+
+    bool SmartSocket::IsOpen() const
+    {
+        return m_socket.next_layer().is_open();
+    };
+
+    string SmartSocket::GetLocalname() const
+    {
+        return m_socket.next_layer().local_endpoint().address().to_string();
+    };
+
+    int SmartSocket::GetLocalPort() const
+    {
+        return m_socket.next_layer().local_endpoint().port();
+    };
+
+    string SmartSocket::GetServername() const
+    {
+        return m_socket.next_layer().remote_endpoint().address().to_string();
+    };
+
+    int SmartSocket::GetServerPort() const
+    {
+        return m_socket.next_layer().remote_endpoint().port();
+    };
+
+    boost::asio::io_context& SmartSocket::GetIoContext()
+    {
+        return m_io_context;
+    };
+
+    bool SmartSocket::SetTimeout(int timeout)
+    {
+        m_timeout = timeout;
+        return true;
+    };
+
+    bool SmartSocket::AsyncConnectCoroutine(const string& server, int port, asio::yield_context& yield)
+    {
+        m_server = server;
+        m_port = port;
+
+        system::error_code ec;
+
+        tcp::resolver::query query(m_server, std::to_string(m_port));
+        tcp::resolver::results_type results = m_resolver.async_resolve(query, yield[ec]);
+        asio::async_connect(m_socket.next_layer(), results.begin(), results.end(), yield[ec]);
+        
+        return ISXLOGS::SmartSocketMethodsHandlers::HandleConnection(GetServername(), GetServerPort(), ec);
+    };
+
+    bool SmartSocket::AsyncWriteCoroutine(const string& data, asio::yield_context& yield)
+    {
+        system::error_code ec;
+        
+        if (!m_ssl_enabled)
+        {
+            asio::async_write(m_socket.next_layer(), asio::buffer(data), yield[ec]);
+        } else
+        {
+            asio::async_write(m_socket, asio::buffer(data), yield[ec]);
+        };
+
+        return ISXLOGS::SmartSocketMethodsHandlers::HandleWrite(data, ec);
+    };
+
+    string SmartSocket::AsyncReadCoroutine(asio::yield_context& yield)
+    {
+        system::error_code ec;
+        asio::streambuf buffer;
+
+        auto timer = StartTimer(m_timeout, yield, ec);
+
+        if (!m_ssl_enabled)
+        {
+            asio::async_read_until(m_socket.next_layer(), buffer, "\r\n", yield[ec]);   
+        } else
+        {
+            asio::async_read_until(m_socket, buffer, "\r\n", yield[ec]);   
+        };
+
+        // Cancel the timer if the read completes first
+        timer->cancel();
+        
+        return ISXLOGS::SmartSocketMethodsHandlers::HandleRead(buffer, ec);
+    };
+
+    bool SmartSocket::AsyncUpgradeSecurityCoroutine(asio::yield_context& yield)
+    {
+        system::error_code ec;
+        m_socket.async_handshake(boost::asio::ssl::stream_base::handshake_type::client, yield[ec]);
+        
+        return ISXLOGS::SmartSocketMethodsHandlers::HandleUpgradeSecurity(ec, &m_ssl_enabled);
+    };
+
+    bool SmartSocket::Close()
+    {
+        system::error_code ec;
+        if (IsOpen())
+        {
+            m_socket.next_layer().close(ec);
+        } else
+        {
+            return true;
+        };
+
+        return ISXLOGS::SmartSocketMethodsHandlers::HandleClose(ec);
+    };
+
+    std::unique_ptr<asio::steady_timer> SmartSocket::StartTimer(
+        int seconds, asio::yield_context& yield, system::error_code& ec)
+    {
+        std::unique_ptr<asio::steady_timer> timer = std::make_unique<asio::steady_timer>(yield.get_executor());
+        timer->expires_after(std::chrono::seconds(seconds));
+        timer->async_wait([&](const system::error_code& error) {
+            if (!error) {
+                if (!m_ssl_enabled) {
+                    m_socket.next_layer().cancel();
+                } else {
+                    m_socket.lowest_layer().cancel();
+                };
+            }
+        });
+
+        return timer;
+    };
+}; // namespace ISXSmartSocket
