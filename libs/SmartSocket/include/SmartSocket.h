52cf55edbeb0d82f4704ecdcdd398599c0ef06c4
vsh51 <volshp125@gmail.com>
2024-08-13 10:45:56 +0300

    Code formatting changes

--- libs/SmartSocket/include/SmartSocket.h
+++ libs/SmartSocket/include/SmartSocket.h
@@ -17,42 +17,42 @@ using std::function;
 
 namespace ISXSmartSocket
 {
-    class SmartSocket
-    {
-    public:
-        SmartSocket(asio::io_context& io_context, asio::ssl::context& ssl_context);
-        ~SmartSocket();
+class SmartSocket
+{
+public:
+    SmartSocket(asio::io_context& io_context, asio::ssl::context& ssl_context);
+    ~SmartSocket();
+    
+    bool IsOpen() const;
+    string GetLocalname() const;
+    int GetLocalPort() const;
+    string GetServername() const;
+    int GetServerPort() const;
+
+    bool SetTimeout(int timeout);
+
+    boost::asio::io_context& GetIoContext();
+
+    bool AsyncConnectCoroutine(const string& server, int port, asio::yield_context& yield);
+    bool AsyncWriteCoroutine(const string& data, asio::yield_context& yield);
+    string AsyncReadCoroutine(asio::yield_context& yield);
+    bool AsyncUpgradeSecurityCoroutine(asio::yield_context& yield);
+    bool Close();
+
+private:
+    std::unique_ptr<asio::steady_timer> StartTimer(
+        int seconds, asio::yield_context& yield, system::error_code& ec);
         
-        bool IsOpen() const;
-        string GetLocalname() const;
-        int GetLocalPort() const;
-        string GetServername() const;
-        int GetServerPort() const;
+    string m_server;
+    int m_port;
 
-        bool SetTimeout(int timeout);
+    bool m_ssl_enabled;
 
-        boost::asio::io_context& GetIoContext();
+    int m_timeout = 5;
 
-        bool AsyncConnectCoroutine(const string& server, int port, asio::yield_context& yield);
-        bool AsyncWriteCoroutine(const string& data, asio::yield_context& yield);
-        string AsyncReadCoroutine(asio::yield_context& yield);
-        bool AsyncUpgradeSecurityCoroutine(asio::yield_context& yield);
-        bool Close();
-
-    private:
-        string m_server;
-        int m_port;
-
-        bool m_ssl_enabled;
-
-        int m_timeout = 5;
-
-        tcp::resolver m_resolver;
-        asio::io_context& m_io_context;
-        asio::ssl::context& m_ssl_context;
-        asio::ssl::stream<tcp::socket> m_socket;
-        
-        std::unique_ptr<asio::steady_timer> StartTimer(
-            int seconds, asio::yield_context& yield, system::error_code& ec);
-    };
+    tcp::resolver m_resolver;
+    asio::io_context& m_io_context;
+    asio::ssl::context& m_ssl_context;
+    asio::ssl::stream<tcp::socket> m_socket;
+};
 }; // namespace ISXSmartSocket
