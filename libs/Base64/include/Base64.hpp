8d7967382ca94f2ff5b3a0dc8f17b29f726fb4e8
mshevtso <mshevtso@cisco.com>
2024-12-30 04:44:53 -0500

    improv: chhange strng to string_view in base64 lib

--- libs/Base64/include/Base64.hpp
+++ libs/Base64/include/Base64.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <boost/beast/core/detail/base64.hpp>
-#include <string>
+#include <string_view>
 
 using std::string;
 
 namespace ISXBase64
 {
-    string Base64Encode(const string& decoded);
+    string Base64Encode(std::string_view decoded);
 
-    string Base64Decode(const string& encoded);
+    string Base64Decode(std::string_view encoded);
 }; // namespace ISXBase64
--- libs/Base64/src/Base64.cpp
+++ libs/Base64/src/Base64.cpp
@@ -2,19 +2,21 @@
 
 namespace ISXBase64
 {
-    string Base64Encode(const string& decoded)
+    // Base64 encoding
+    std::string Base64Encode(std::string_view decoded)
     {
         auto encoded_size = boost::beast::detail::base64::encoded_size(decoded.size());
         std::string encoded_output(encoded_size, '\0');
         boost::beast::detail::base64::encode(encoded_output.data(), decoded.data(), decoded.size());
         return encoded_output;
-    };
+    }
 
-    string Base64Decode(const string& encoded)
+    // Base64 decoding
+    std::string Base64Decode(std::string_view encoded)
     {
         auto decoded_size = boost::beast::detail::base64::decoded_size(encoded.size());
         std::string decoded_output(decoded_size, '\0');
         boost::beast::detail::base64::decode(decoded_output.data(), encoded.data(), encoded.size());
         return decoded_output;
-    };
-}; // namespace ISXBase64
+    }
+} // namespace ISXBase64
